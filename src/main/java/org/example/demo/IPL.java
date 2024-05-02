fedee368da998040d206813f5431cb29183b5b6d
Raghu <kraghuveertdl133@gmail.com>
2024-05-02 22:35:30 +0530

    master new project added

---
+++ src/main/java/org/example/demo/IPL.java
@@ -0,0 +1,14 @@
+package org.example.demo;
+
+public class IPL {
+    public static void main(String[] args) {
+        int CSK = 5;
+        int MI = 5;
+        int RCB = 0;
+        int KKR = 2;
+
+        if(RCB==0){
+            System.out.println("Ee sala cup namde!!!");
+        }
+    }
+}
